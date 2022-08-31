d3a6266d7f8ce2809d4b6f33cd324d7a084def7e
mdwilkerson <112137869+mdwilkerson@users.noreply.github.com>
2022-08-31 10:51:28 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        double x = 2.5;
+        double y = 3.3;
+         double z = 34.9;
+         
+         z = 4 * x * x + 3 * y;
+         Console.WriteLine($"The value of z:{z}");
+         Console.WriteLine("X ="+x, "Y = "+y);
+         Console.WriteLine("The value of Z is:"+z);
+
+         
+
+
+
+    }
+}
